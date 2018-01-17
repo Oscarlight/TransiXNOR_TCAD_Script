87761681706cea3cb76fe0e16435e272d7ab04aa
Oscarlight <oscarli2716@gmail.com>
2018-01-17 11:34:54 -0500

    move TCAD to seperated repo

--- TCAD_files/TransiXNOR/gtooldb.tcl
+++
@@ -1,7 +0,0 @@
-global WB_tool
-#TOOL BEGIN sdevice
-lappend WB_tool(sdevice,output,files) *n@node@_des.plt
-lappend WB_tool(sdevice,output,files) *n@node@_*_des.dat
-lappend WB_tool(sdevice,output,files) *n@node@_*_des.sav
-lappend WB_tool(sdevice,output,files) *n@node@_des.tdr
-#TOOL END
